dcb52ac81154efde634a717cd5e4a6c6d8b6c42b
Topherian <topherianlimited@foo.com>
2017-04-06 17:32:18 +0700

    Module 3 work in progress, a few bugs

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -4,25 +4,21 @@
 angular.module('NarrowItDownApp', [])
 .controller('NarrowItDownController', NarrowItDownController)
 .service('MenuSearchService', MenuSearchService)
-//.directive('foundItems', FoundItemsDirective)
+.directive('foundItems', FoundItemsDirective)
 .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
 
 
-// function FoundItemsDirective() {
-//   var ddo = {
-//     templateUrl: 'shoppingList.html',
-//     scope: {
-//       items: '<',
-//       title: '@'
-//     },
-//     // controller: 'ShoppingListDirectiveController as list',
-//     //controller: ShoppingListDirectiveController,
-//     //controllerAs: 'list',
-//     //bindToController: true
-//   };
-//
-//   return ddo;
-// }
+function FoundItemsDirective() {
+  var ddo = {
+    templateUrl: 'loader/itemsloaderindicator.template.html',
+    scope: {
+      found: '<',
+      onRemove: '&'
+    }
+  };
+
+  return ddo;
+}
 
 
 NarrowItDownController.$inject = ['MenuSearchService'];
@@ -35,29 +31,66 @@ function NarrowItDownController(MenuSearchService) {
   narrowListCtrl.getMatchedMenuItems = function (searchTerm) {
     MenuSearchService.getMatchedMenuItems(searchTerm);
   };
+
+  narrowListCtrl.removeItem = function (itemIndex) {
+    MenuSearchService.removeItem(itemIndex);
+  };
+
+  // narrowListCtrl.filteredItems = function () {
+  //   console.log("In controller filterItems property, calling service to find found items...")
+  //   MenuSearchService.getFoundItems();
+  // };
+
+  narrowListCtrl.filteredItems = MenuSearchService.getFoundItems();
 }
 
 MenuSearchService.$inject = ['$http', 'ApiBasePath'];
 function MenuSearchService($http, ApiBasePath) {
   console.log("Initing MenuSearchService...");
   var service = this;
+  var matchedMenuItems = [];
+
+
+  service.removeItem = function (itemIndex) {
+      matchedMenuItems.splice(itemIndex, 1);
+  };
+
+  service.getFoundItems = function () {
+    console.log("Calling Get method on service: Matched menu items size: " + matchedMenuItems.length);
+    return matchedMenuItems;
+  };
 
   service.getMatchedMenuItems = function (searchTerm) {
-    console.log("Searching for term: " + searchTerm);
+    //matchedMenuItems = [];
+    console.log("Matched menu items size: " + matchedMenuItems.length);
 
+    console.log("Searching for term: " + searchTerm);
     var promise = $http({
       method: "GET",
       url: (ApiBasePath + "/menu_items.json"),
 
     });
 
-    var matchedMenuItems = promise.then(function (response) {
-        console.log("Response data: " + response.data);
-        //TODO filter the results
+    promise.then(function (response) {
+      var results = response.data;
+      console.log("Filtering data.. first, setting matched items to empty..")
+      //matchedMenuItems = [];
+      for(var menuItems in results) {
+        var menuItem = results[menuItems];
+        for(var menuProperty in menuItem) {
+          var menu = menuItem[menuProperty];
+          var foundSearchTerm = menu.description.search(searchTerm.toLowerCase());
+          if (foundSearchTerm != -1) {
+            console.log("adding menu item..");
+            matchedMenuItems.push(menu);
+          }
+        }
+      }
+      console.log("Filtering data completed, matched: " + matchedMenuItems.length);
     })
-      .catch(function (error) {
-        console.log("Something went terribly wrong.");
-      });
+    .catch(function (error) {
+      console.log("Something went terribly wrong.");
+    });
   };
 }
 
